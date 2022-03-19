363524b6a9f12d787bf34864e9162e992bb8ff15
jb <jessb.epicodus@gmail.com>
2022-03-19 16:08:25 -0700

    add delete to engineers/index

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,7 +7,6 @@ namespace Factory.Models {
       this.JoinEntities = new HashSet<EngineerMachine>();
     }
     public int MachineId { get; set; }
-    public DateTime Date { get; set; }
     public string MakeModel { get; set; }
     public string SerialId { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
